b7f1c387f217ab2978896c805461658f9037755e
LostForsaked <ruarteag@gmail.com>
2025-04-15 16:24:54 -0400

    ejercicios 1

--- hola.py
+++ hola.py
@@ -20,8 +20,36 @@
 # print(n1+n2)
 
 
-#multiplicación de numeros
-n1=int(input("ingrese un numero"))
-n2=int(input("ingrese un numero"))
+# #multiplicación de numeros
+# n1=int(input("ingrese un numero"))
+# n2=int(input("ingrese un numero"))
 
-print("el resultado de la multiplicación es", n1*n2)
+# print("el resultado de la multiplicación es", n1*n2)
+
+# # promedio de tres numeros
+# n1=float(input("ingrese un numero "))
+# n2=float(input("ingrese un numero "))
+# n3=float(input("ingrese un numero "))
+# prom=float((n1+n2+n3)/3)
+# print("el promedio es", prom)
+# if prom >= 4.0:
+#     print("aprobado")
+# else:
+#     print("reprobado")
+
+# edad=int(input("ingrese su edad "))
+# if edad>=18:
+#     print("ud es mayor de edad")
+# else:
+#     print("ud no es mayor de edad")
+
+# edad=int(input("ingrese su edad "))
+
+# if edad<12:
+#     print("es usted un niño")
+# elif edad>=12 and edad<18:
+#     print("ud es un adolescente")
+# elif edad>=18 and edad<65:
+#     print("ud es adulto")
+# else:
+#     print("usted es viejito")
