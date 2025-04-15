f828767a3bdc93be3beb09460edbc50f5d396412
LostForsaked <ruarteag@gmail.com>
2025-04-15 15:38:19 -0400

    primer commit

---
+++ hola.py
@@ -0,0 +1,27 @@
+# #declaración de variables
+# nombre="Link"
+# edad=64
+# #ejemplo de concatenación
+# print("Hola", nombre, "y su edad es", edad)
+
+
+# #ingreso de variables para concatenar
+# print("ingrese su nombre")
+# nombre=input()
+# print("ingrese su edad")
+# edad=input()
+# #ejemplo de concatenación
+# print("Hola", nombre, "y su edad es", edad)
+
+
+# # suma de numeros
+# n1=int(input("ingrese un número "))
+# n2=int(input("ingrese un número "))
+# print(n1+n2)
+
+
+#multiplicación de numeros
+n1=int(input("ingrese un numero"))
+n2=int(input("ingrese un numero"))
+
+print("el resultado de la multiplicación es", n1*n2)
